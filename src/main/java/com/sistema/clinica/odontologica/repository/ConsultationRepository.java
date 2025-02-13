653974be30e1f3278460219d09cb2dd3cd6d3f6d
guidias2002 <guidias200220@gmail.com>
2025-02-13 18:12:47 -0300

    feat: create method findAllConsultationByPatientCpf

--- src/main/java/com/sistema/clinica/odontologica/repository/ConsultationRepository.java
+++ src/main/java/com/sistema/clinica/odontologica/repository/ConsultationRepository.java
@@ -3,5 +3,9 @@ package com.sistema.clinica.odontologica.repository;
 import com.sistema.clinica.odontologica.domain.ConsultationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ConsultationRepository extends JpaRepository<ConsultationEntity, Long> {
+
+    List<ConsultationEntity> findAllConsultationByPatientCpf(String patientCpf);
 }
