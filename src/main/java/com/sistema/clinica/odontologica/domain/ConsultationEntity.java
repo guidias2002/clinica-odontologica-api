859f9c0bfbf66252d2c70c8e77329371ac80d40c
guidias2002 <guidias200220@gmail.com>
2025-02-13 16:13:54 -0300

    feat: create ConsultationEntity

---
+++ src/main/java/com/sistema/clinica/odontologica/domain/ConsultationEntity.java
@@ -0,0 +1,42 @@
+package com.sistema.clinica.odontologica.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "consulta")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConsultationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String patientName;
+    private String patientEmail;
+    private String patientCellphone;
+    private String patientDocument;
+    private String dentistName;
+    private LocalDateTime consultationDate;
+    private String consultationType;
+    private int estimatedDuration;
+
+    @Enumerated(EnumType.STRING)
+    private ConsultationStatus consultationStatus;
+    private String observations;
+    private BigDecimal consultationValue;
+    private String paymentMethod;
+    private String paymentStatus;
+    private boolean notificationSent;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
