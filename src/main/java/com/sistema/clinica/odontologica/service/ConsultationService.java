5034c4e2c8a0c71029c0b6ccbfd63e8f119e9c96
guidias2002 <guidias200220@gmail.com>
2025-02-13 17:47:34 -0300

    feat: create ConsultationService

--- src/main/java/com/sistema/clinica/odontologica/service/ConsultationService.java
+++ src/main/java/com/sistema/clinica/odontologica/service/ConsultationService.java
@@ -1,10 +1,14 @@
 package com.sistema.clinica.odontologica.service;
 
+import com.sistema.clinica.odontologica.domain.ConsultationEntity;
 import com.sistema.clinica.odontologica.dto.ConsultationDto;
 import com.sistema.clinica.odontologica.mapper.ConsultationMapper;
 import com.sistema.clinica.odontologica.repository.ConsultationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
 
 @Service
 public class ConsultationService {
@@ -16,8 +20,13 @@ public class ConsultationService {
     private ConsultationMapper consultationMapper;
 
     public ConsultationDto saveConsultation(ConsultationDto consultationDto) {
-        this.consultationRepository.save(consultationMapper.toConsultationEntity(consultationDto));
+        ConsultationEntity newConsultation = consultationMapper.toConsultationEntity(consultationDto);
 
-        return consultationDto;
+        newConsultation.setCreatedAt(LocalDateTime.now());
+        newConsultation.setUpdatedAt(LocalDateTime.now());
+
+        this.consultationRepository.save(newConsultation);
+
+        return consultationMapper.toConsultationDto(newConsultation);
     }
 }
