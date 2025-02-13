ac6447635915ccae9281d8b0f7e749ff7fe495c5
guidias2002 <guidias200220@gmail.com>
2025-02-13 16:14:36 -0300

    feat: create PatientEntity

---
+++ src/main/java/com/sistema/clinica/odontologica/domain/PatientEntity.java
@@ -0,0 +1,32 @@
+package com.sistema.clinica.odontologica.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "patient")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PatientEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String cpf;
+
+    @Column(unique = true)
+    private String email;
+
+    private LocalDateTime createdAt;
+}
