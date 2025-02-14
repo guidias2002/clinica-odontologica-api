75facf1e63667ef3ee5d97e19d3816029e3f3c15
guidias2002 <guidias200220@gmail.com>
2025-02-14 16:06:42 -0300

    feat: create AvailableTimeService

---
+++ src/main/java/com/sistema/clinica/odontologica/service/AvailableTimeService.java
@@ -0,0 +1,56 @@
+package com.sistema.clinica.odontologica.service;
+
+import com.sistema.clinica.odontologica.domain.AvailableTimeEntity;
+import com.sistema.clinica.odontologica.domain.ProfessionalEntity;
+import com.sistema.clinica.odontologica.dto.AvailableTimeDto;
+import com.sistema.clinica.odontologica.mapper.AvailableTimeMapper;
+import com.sistema.clinica.odontologica.mapper.ProfessionalMapper;
+import com.sistema.clinica.odontologica.repository.AvailableTimeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AvailableTimeService {
+
+    @Autowired
+    private AvailableTimeRepository availableTimeRepository;
+
+    @Autowired
+    private AvailableTimeMapper availableTimeMapper;
+
+    @Autowired
+    private ProfessionalService professionalService;
+
+    @Autowired
+    private ProfessionalMapper professionalMapper;
+
+    public List<String> getAvailableTimes(Long professionalId, LocalDate date) {
+        List<AvailableTimeEntity> availableTimes = availableTimeRepository
+                .findByProfessionalIdAndDateAndBookedFalse(professionalId, date);
+
+        return availableTimes.stream()
+                .map(time -> time.getTime().toString()) // retorna a String do horário
+                .collect(Collectors.toList());
+    }
+
+    public AvailableTimeDto reserveTime(Long professionaId, LocalDate date, LocalTime time) {
+        ProfessionalEntity professionalEntity = professionalMapper.toProfessionalEntity(professionalService.getProfessionalById(professionaId));
+
+        // retorna um availableTimeEntity pela data e hora enviada
+        AvailableTimeEntity availableTimeEntity = availableTimeRepository.findByDateAndTime(date, time);
+
+        if(availableTimeEntity.isBooked()) {
+            throw new RuntimeException("Esse horário já foi reservado.");
+        }
+
+        availableTimeEntity.setBooked(true);
+        availableTimeRepository.save(availableTimeEntity);
+
+        return availableTimeMapper.toAvailableTimeDto(availableTimeEntity);
+    }
+}
