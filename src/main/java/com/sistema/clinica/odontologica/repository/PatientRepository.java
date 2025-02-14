38b21e60ed36097a1b677e67d3e3a36ba603c646
guidias2002 <guidias200220@gmail.com>
2025-02-14 16:09:16 -0300

    feat: create PatientRepository

--- src/main/java/com/sistema/clinica/odontologica/repository/PatientRepository.java
+++ src/main/java/com/sistema/clinica/odontologica/repository/PatientRepository.java
@@ -2,9 +2,11 @@ package com.sistema.clinica.odontologica.repository;
 
 import com.sistema.clinica.odontologica.domain.PatientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PatientRepository extends JpaRepository<PatientEntity, Long> {
 
     Optional<PatientEntity> findPatientByCpf(String cpf);
