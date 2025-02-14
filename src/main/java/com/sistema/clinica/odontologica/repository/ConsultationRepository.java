112adade055cace4958d7f8bab91412359ce7567
guidias2002 <guidias200220@gmail.com>
2025-02-14 16:08:43 -0300

    feat: craete findByProfessionalIdAndConsultationDate method

--- src/main/java/com/sistema/clinica/odontologica/repository/ConsultationRepository.java
+++ src/main/java/com/sistema/clinica/odontologica/repository/ConsultationRepository.java
@@ -1,11 +1,19 @@
 package com.sistema.clinica.odontologica.repository;
 
 import com.sistema.clinica.odontologica.domain.ConsultationEntity;
+import com.sistema.clinica.odontologica.domain.ConsultationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface ConsultationRepository extends JpaRepository<ConsultationEntity, Long> {
 
     List<ConsultationEntity> findAllConsultationByPatientCpf(String patientCpf);
+
+    List<ConsultationEntity> findAllConsultationByStatus(ConsultationStatus status);
+
+    List<ConsultationEntity> findByProfessionalIdAndConsultationDate(Long professionalId, LocalDate date);
 }
