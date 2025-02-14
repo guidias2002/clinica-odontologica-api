c93e419cd8b6e906ba7030be85cb8dbdcefad445
guidias2002 <guidias200220@gmail.com>
2025-02-14 16:09:37 -0300

    feat: create ProfessionalEntity

---
+++ src/main/java/com/sistema/clinica/odontologica/domain/ProfessionalEntity.java
@@ -0,0 +1,33 @@
+package com.sistema.clinica.odontologica.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "professional")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfessionalEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "professional", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AvailableTimeEntity> availableTimes = new ArrayList<>();
+}
+
