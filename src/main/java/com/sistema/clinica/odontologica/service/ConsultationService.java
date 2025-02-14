06fe3a1e464940b5f5d17b8e7c4aa05098581482
guidias2002 <guidias200220@gmail.com>
2025-02-13 22:22:27 -0300

    feat: check if there is a patient registered with the CPF added in the findAllConsultationByPatientCpf method

--- src/main/java/com/sistema/clinica/odontologica/service/ConsultationService.java
+++ src/main/java/com/sistema/clinica/odontologica/service/ConsultationService.java
@@ -1,7 +1,9 @@
 package com.sistema.clinica.odontologica.service;
 
 import com.sistema.clinica.odontologica.domain.ConsultationEntity;
+import com.sistema.clinica.odontologica.domain.PatientEntity;
 import com.sistema.clinica.odontologica.dto.ConsultationDto;
+import com.sistema.clinica.odontologica.dto.PatientDto;
 import com.sistema.clinica.odontologica.mapper.ConsultationMapper;
 import com.sistema.clinica.odontologica.repository.ConsultationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,9 @@ public class ConsultationService {
 
     @Autowired
     private ConsultationMapper consultationMapper;
+
+    @Autowired
+    private PatientService patientService;
 
     public ConsultationDto saveConsultation(ConsultationDto consultationDto) {
         ConsultationEntity newConsultation = consultationMapper.toConsultationEntity(consultationDto);
@@ -38,7 +43,9 @@ public class ConsultationService {
     }
 
     public List<ConsultationDto> getAllConsultationsByPatient(String patientCpf) {
-        List<ConsultationEntity> consultationEntityList = consultationRepository.findAllConsultationByPatientCpf(patientCpf);
+        PatientDto patient = patientService.findPatientByCpf(patientCpf);
+
+        List<ConsultationEntity> consultationEntityList = consultationRepository.findAllConsultationByPatientCpf(patient.cpf());
 
         return consultationMapper.toConsultationDtoList(consultationEntityList);
     }
