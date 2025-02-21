3b2f2fc50cd2e8899c9ad7943e6b91aa5d34c037
guidias2002 <guidias200220@gmail.com>
2025-02-21 18:37:50 -0300

    feat: create toProfessionalBasicInfoDtoList method

--- src/main/java/com/sistema/clinica/odontologica/mapper/ProfessionalMapper.java
+++ src/main/java/com/sistema/clinica/odontologica/mapper/ProfessionalMapper.java
@@ -1,8 +1,11 @@
 package com.sistema.clinica.odontologica.mapper;
 
 import com.sistema.clinica.odontologica.domain.ProfessionalEntity;
+import com.sistema.clinica.odontologica.dto.ProfessionalBasicInfoDto;
 import com.sistema.clinica.odontologica.dto.ProfessionalDto;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProfessionalMapper {
@@ -10,4 +13,6 @@ public interface ProfessionalMapper {
     ProfessionalEntity toProfessionalEntity(ProfessionalDto professionalDto);
 
     ProfessionalDto toProfessionalDto(ProfessionalEntity professionalEntity);
+
+    List<ProfessionalBasicInfoDto> toProfessionalBasicInfoDtoList(List<ProfessionalEntity> professionalEntityList);
 }
